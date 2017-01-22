fedf17171049f9ceabd488eb672088566e6e1a4e
Paul Ogden <ageoldpun@gmail.com>
2017-01-21 16:30:44 -0800

    Login and navigate

--- klassy_melania.js
+++ klassy_melania.js
@@ -12,5 +12,19 @@ var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
+var klassyPhotos = [
+  'http://gq-images.condecdn.net/image/E8GdknR5L1W/crop/1620',
+  'http://gq-images.condecdn.net/image/El2R7DjGzrl/crop/1020',
+  'http://gq-images.condecdn.net/image/rp9e4GqrlDq/crop/1020',
+  'http://gq-images.condecdn.net/image/jvRL9DO5y93/crop/1020',
+  'http://gq-images.condecdn.net/image/4732EJPGzo2/crop/1020',
+  'http://gq-images.condecdn.net/image/7wqNvNmwppy/crop/1020'
+];
+
 
 driver.get('http://www.facebook.com/');
+driver.findElement(By.id('email')).sendKeys(process.env.FACEBOOK_EMAIL);
+driver.findElement(By.id('pass')).sendKeys(process.env.FACEBOOK_PASSWORD);
+driver.findElement(By.id('loginbutton')).click();
+driver.get(process.env.FACEBOOK_TARGET_URL);
+driver.quit()
