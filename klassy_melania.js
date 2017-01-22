4b6f0d936b9fcbe420bc2b9ca3131a9dba528cd8
Paul Ogden <ageoldpun@gmail.com>
2017-01-21 17:23:50 -0800

    Parse the post text

--- klassy_melania.js
+++ klassy_melania.js
@@ -12,6 +12,12 @@ var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
+var klassySayings = [
+  'Such Klass! ',
+  'Can you get over how much klass she has? ',
+  'More klass than you can shake a stick at. '
+];
+
 var klassyPhotos = [
   'http://gq-images.condecdn.net/image/E8GdknR5L1W/crop/1620',
   'http://gq-images.condecdn.net/image/El2R7DjGzrl/crop/1020',
@@ -22,9 +28,25 @@ var klassyPhotos = [
 ];
 
 
+// Log in and navigate to targets page
 driver.get('http://www.facebook.com/');
 driver.findElement(By.id('email')).sendKeys(process.env.FACEBOOK_EMAIL);
 driver.findElement(By.id('pass')).sendKeys(process.env.FACEBOOK_PASSWORD);
 driver.findElement(By.id('loginbutton')).click();
 driver.get(process.env.FACEBOOK_TARGET_URL);
-driver.quit()
+
+// Find if latest post is about Melania
+var postElement = driver.findElement(By.className('userContent'))
+var postChild = postElement.findElement(By.xpath("child::*"));
+return postChild.getText()
+.then(function(postText) {
+  var postWords = postText.split(' ');
+  postWords.forEach(function(word) {
+    if (word.toLowerCase() === 'melania' || word.toLowerCase() === 'classy') {
+      // Make the post!
+      var post = klassySayings[Math.floor(Math.random()*klassySayings.length)] +
+        klassyPhotos[Math.floor(Math.random()*klassyPhotos.length)];
+    }
+  })
+  //driver.quit()
+})
